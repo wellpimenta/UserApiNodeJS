82abded12f1c51348271e79ca3de04b1036e90c4
wellpimenta <wellpimenta@yahoo.com.br>
2024-11-04 06:42:33 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,1 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-});
-
-sequelize.authenticate()
-  .then(() => console.log('Conectado ao MySQL com sucesso!'))
-  .catch(err => console.log('Erro ao conectar ao MySQL:', err));
-
-module.exports = sequelize;
+.
